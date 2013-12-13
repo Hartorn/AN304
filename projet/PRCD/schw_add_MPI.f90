19103e9066a0f07db9b3f80f88807c8cfe696b4e
Hartorn <bazirehoussin@gmail.com>
2013-12-13 10:03:10 +0100

    probleme calcul erreur

--- projet/PRCD/schw_add_MPI.f90
+++ projet/PRCD/schw_add_MPI.f90
@@ -9,7 +9,7 @@ program schwarz_additif
 
 	! Declaration des variables pour schwarz
 	real*8, parameter  :: eps = 1.d-12
-	real*8	:: ERROR,ERR_glob
+	real*8	:: error_calcul ,ERR_glob
 	! Decalaration des variables MPI et charges	
         integer	:: itop,ibottom	 !indice de localisation des debuts de msg a envoyer
 	! Declaration des variables numeriques
@@ -100,7 +100,7 @@ program schwarz_additif
     U(1:N)    = 0.0d0
 	Utop(1:Nx)  = 0.0d0
 	Ubottom(1:Nx)  = 0.0d0
-	error=1
+	error_calcul=1
 
 !	schw_iter = 0
 	ERR_glob = 1.d3
@@ -143,12 +143,12 @@ program schwarz_additif
 		call MPI_Wait(recv_top, IERROR)
 		call MPI_Wait(recv_bottom, IERROR)
 		endif
-        error=1	
-	!write(*,*) 'debut boucle err:', error,' j:', j
+        error_calcul=1	
+	!write(*,*) 'debut boucle err:', error_calcul,' j:', j
 
 	! Boucle tant que non convergence
-	Do while ((error > eps) .AND. (j < max_iter))
-	write(*,*) 'debut boucle err:', error,' j:', j
+	Do while ((error_calcul > eps) .AND. (j < max_iter))
+	write(*,*) 'debut boucle err:', error_calcul,' j:', j
 		! Communication entre les vecteurs
 		if (myrank == 0)then
 		call MPI_Start(send_top, IERROR)
@@ -188,11 +188,11 @@ program schwarz_additif
 		!calcul de l erreur
 		Uold(ibottom:itop+Nx-1)=U(ibottom:itop+Nx-1)-Uold(ibottom:itop+Nx-1)
         error=sqrt(dot_product(Uold(ibottom:itop+Nx-1),Uold(ibottom:itop+Nx-1)))
-        write(*,*)'avant allreduce err :', error		
+        write(*,*)'avant allreduce err :', error_calcul		
 		
 		! Test de verification de convergence (MPI_all_reduce)
-		call MPI_Allreduce(MPI_IN_PLACE, error,1,  MPI_DOUBLE_PRECISION, MPI_MAX, MPI_COMM_WORLD, IERROR)
-		write(*,*)'apres allreduce err :', error	
+		call MPI_Allreduce(MPI_IN_PLACE, error_calcul,1,  MPI_DOUBLE_PRECISION, MPI_MAX, MPI_COMM_WORLD, IERROR)
+		write(*,*)'apres allreduce err :', error_calcul	
 		j=j+1
 		
 		! Attente de la fin des receptions
@@ -204,9 +204,9 @@ program schwarz_additif
 			call MPI_Wait(recv_top, IERROR)
 			call MPI_Wait(recv_bottom, IERROR)
 		endif
-		write(*,*)'fin boucle :', error, 'j:', j	
+		write(*,*)'fin boucle :', error_calcul, 'j:', j	
 	ENDDO
-		write(*,*) 'fin , convergence en ', j, 'iterations', error
+		write(*,*) 'fin , convergence en ', j, 'iterations', error_calcul
 !~ 		call jacobi(Aii,Cx,Cy,Nx,1,N,RHS,U,Uold,l)
 
 	call wrisol( U,Nx,Ny,dx,dy,77,1,N )
